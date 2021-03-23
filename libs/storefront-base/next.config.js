d28ec1903f5592c8425d1b242882e1720f90f771
Nguyen Thi Loan <loan9202@gmail.com>
2021-03-23 11:18:35 +0700

    config pwa

--- libs/storefront-base/next.config.js
+++ libs/storefront-base/next.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa')
+module.exports = withPWA({
   env: {
     PUBLIC_URL: "",
     PORT: 9000,
     ADMIN_URL: "http://localhost:3000",
     STRAPI_URL: "http://localhost:1337"
+  },
+  pwa: {
+    dest: 'public'
   }
-};
+});
